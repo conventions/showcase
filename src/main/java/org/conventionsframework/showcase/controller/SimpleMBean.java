96c284553b68662ba12542635f902f9b0d5f160e
rmpestano <rmpestano@rmpestano-VPCEG15FB>
2013-02-25 11:20:56 -0300

    - sample update

--- src/main/java/org/conventionsframework/showcase/controller/SimpleMBean.java
+++ src/main/java/org/conventionsframework/showcase/controller/SimpleMBean.java
@@ -20,12 +20,16 @@ import org.apache.myfaces.extensions.cdi.core.api.scope.conversation.ViewAccessS
 @ViewAccessScoped
 @PersistentClass(Person.class)//managed bean entity, you can use create() method instead
 @Service(name=Service.STATEFUL,entity=Person.class)//same as commented setService() method
+//@Service(type = Type.STATEFUL,entity = Person.class)//also same as commented setService() method
+//both @Service will Inject a generic service with an extended PersistenceContext to CRUD Person entity
+//@Service(PersonService.class) //inject service by type, in this case you dont need entity attribute 
+//cause PersonService is not generic, it is specialized in Person entity 
 public class SimpleMBean extends BaseMBean<Person> {
 
     
-    /**
+     /**
      * this method is REQUIRED to tell the framework how to 'crud' the managed bean's entity
-     * or use @Service annotation to provide the name of an existing service @see PersonMBean
+     * or use @Service annotation to provide the name,type or value of an existing service 
      * @param baseService
      */
 //    @Inject 
